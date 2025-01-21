c992c207bb2520c99753c978349789a49e95aadb
elene <elene.luyckx@student.howest.be>
2025-01-21 21:50:21 +0100

    add horizontal scroll

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
 
+{
+  gsap.registerPlugin(ScrollTrigger);
 
+  const horizontalText = () => {
+    gsap.fromTo(".p_horizontal--scroll", { x: "80%", }, {
+      x: "-30%",
+      scrollTrigger: {
+        trigger: ".job_corrector",
+        start: "top 30%",
+        end: "bottom 30%",
+        markers: {
+          startColor: "purple",
+          endColor: "fuchsia",
+          fontSize: "1rem",
+          indent: 100,
+          fontWeight: "bold",
+        },
+        scrub: 0.5,
+        pin: "p_horizontal--scroll",
+      },
+    });
+  }
+
+
+
+  const init = async () => {
+    horizontalText();
+  };
+  init();
+}
 
