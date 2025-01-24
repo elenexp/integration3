b773c24e5678c2dbd6b85955697a72720472710d
elene <elene.luyckx@student.howest.be>
2025-01-25 00:19:11 +0100

    remove background shape from assets, style printer section, js for compositor interaction

--- js/main.js
+++ js/main.js
@@ -2,24 +2,24 @@
 {
 
   let earnings = 0;
-  let error_state = 0;
+  let errorState = 0;
+  let letterState = 0;
 
   const speakHandler = () => {
-    //const text = event.currentTarget.dataset.text;
     const speech = new SpeechSynthesisUtterance(`In the beginning God created the heavens and the earth. And the earth was without form, and void; and darkness was upon the face of the deep. And the Spirit of God moved upon the face of the waters. And God said, 'Let there be light:' and there was light.`);
     speechSynthesis.speak(speech);
   }
 
   const checkError = (event) => {
     if (event.currentTarget.classList.contains('error_word') && !event.currentTarget.classList.contains('corrected')) {
-      error_state += 1
-      console.log(error_state);
+      errorState += 1
+      console.log(errorState);
       event.currentTarget.classList.add('pink');
       event.currentTarget.classList.add('corrected');
 
       const $errorNumber = document.querySelector(`.error_state--number`);
-      $errorNumber.innerText = `${error_state}`;
-      checkErrorCompleted();
+      $errorNumber.innerText = `${errorState}`;
+      addEarnings(errorState, 3, 10);
     }
 
   }
@@ -29,7 +29,37 @@
 
 
     if (error_state === 3) {
-      earnings =+ 10
+      earnings = + 10
+    }
+
+    $earningsNumber.innerText = `${earnings}`;
+  }
+
+  const stopShaking = (event) => {
+
+
+    if (event.currentTarget.classList.contains('shaking')) {
+      event.currentTarget.classList.remove('shaking');
+      event.currentTarget.classList.remove('shaking_fast');
+      letterState += 1
+    }
+
+    if (event.currentTarget.classList.contains('shaking_fast')) {
+      event.currentTarget.classList.add('shaking');
+      event.currentTarget.classList.remove('shaking_fast');
+    }
+    
+    const $letterNumber = document.querySelector(`.letter_state--number`);
+    $letterNumber.innerText = `${letterState}`;
+    addEarnings(letterState, 6, 10);
+  }
+
+  const addEarnings = (state, stateNumber, amount) => {
+    const $earningsNumber = document.querySelector(`.earnings--number`);
+
+    if (state === stateNumber) {
+      earnings = earnings + amount;
+      console.log('hi')
     }
 
     $earningsNumber.innerText = `${earnings}`;
@@ -45,6 +75,13 @@
 
     $words.forEach($word => {
       $word.addEventListener(`click`, checkError);
+
+    })
+
+    const $letters = document.querySelectorAll(`.shaking_letter`);
+
+    $letters.forEach($letter => {
+      $letter.addEventListener(`click`, stopShaking);
 
     })
 
